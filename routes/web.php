ff81e620c132e44963c4e9f1557f207a0419289c
Ilham J.E <ilham5566@gmail.com>
2024-03-18 15:47:48 +0800

    feat:login

--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+Route::redirect('/', '/login', 201);
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
