4f44c9fe05a1fa1ab4d8ea4ee107a8cee4e78770
Ilham J.E <ilham5566@gmail.com>
2024-03-19 10:55:48 +0800

    feat : dashboard 2

--- resources/js/Pages/Prototype/Dashboard.jsx
+++ resources/js/Pages/Prototype/Dashboard.jsx
@@ -1,5 +1,6 @@
 import Authenticated from "@/Layouts/Authenticated/index";
 import Flickity from "react-flickity-component";
+import { Head, Link } from "@inertiajs/react";
 
 export default function Dashboard() {
     const flickityOptions = {
@@ -14,40 +15,48 @@ export default function Dashboard() {
 
     return (
         <Authenticated>
-            {" "}
+            <Head>
+                <link
+                    rel="stylesheet"
+                    href="https://unpkg.com/flickity@2/dist/flickity.min.css"
+                />
+            </Head>
             <div>
-                <div class="font-semibold text-[22px] text-black mb-4">
+                <div className="font-semibold text-[22px] text-black mb-4">
                     Featured Movies
                 </div>
-                <Flickity class="gap-[30px]" options={flickityOptions}>
+                <Flickity className="gap-[30px]" options={flickityOptions}>
                     {[1, 2, 3, 4].map((i) => (
                         // {/* Movie Thumbnail  */}
-                        <div class="absolute overflow-hidden group mr-[30px]">
+                        <div
+                            className="absolute overflow-hidden group mr-[30px]"
+                            key={i}
+                        >
                             <img
                                 src="/images/featured-1.png"
-                                class="object-cover rounded-[30px] w-[520px] h-[340px]"
+                                className="object-cover rounded-[30px] w-[520px] h-[340px]"
                                 alt=""
                             />
                             {/* rating */}
-                            <div class="rating absolute top-0 left-0">
-                                <div class="p-[30px] flex items-center gap-1">
+                            <div className="rating absolute top-0 left-0">
+                                <div className="p-[30px] flex items-center gap-1">
                                     <img src="/icons/ic_star.svg" alt="" />
-                                    <span class="text-sm font-medium text-white mt-1">
+                                    <span className="text-sm font-medium text-white mt-1">
                                         4.5/5.0
                                     </span>
                                 </div>
                             </div>
                             {/* bottom detail */}
-                            <div class="absolute bottom-0 h-[100px] left-0 right-0 bg-gradient-to-t from-black rounded-bl-[28px] rounded-br-[28px] flex justify-between items-center px-7 h-[130px]">
+                            <div className="absolute bottom-0 h-[100px] left-0 right-0 bg-gradient-to-t from-black rounded-bl-[28px] rounded-br-[28px] flex justify-between items-center px-7 h-[130px]">
                                 <div>
-                                    <div class="font-medium text-[22px] text-white">
+                                    <div className="font-medium text-[22px] text-white">
                                         The Batman in Love
                                     </div>
-                                    <p class="mb-0 text-white text-sm font-light">
+                                    <p className="mb-0 text-white text-sm font-light">
                                         Action • Horror
                                     </p>
                                 </div>
-                                <div class="translate-x-[100px] group-hover:translate-x-0 transition ease-in-out duration-500">
+                                <div className="translate-x-[100px] group-hover:translate-x-0 transition ease-in-out duration-500">
                                     <img
                                         src="/icons/ic_play.svg"
                                         width="50"
@@ -57,7 +66,7 @@ export default function Dashboard() {
                             </div>
                             <a
                                 href="watching.html"
-                                class="inset-0 absolute z-50"
+                                className="inset-0 absolute z-50"
                             ></a>
                         </div>
                     ))}
